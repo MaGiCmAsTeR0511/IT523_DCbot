87ff4a544792f8ea372da46d69af3e2f8d60feed
MaGiCmAsTeR0511 <kurt@hohenauers.eu>
2021-05-07 10:21:04 +0200

    added cronjob function for periodically send out of courses added Details function for priuvate messages updated README.md

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ var task = cron.schedule('0 * * * * 5', () => {
     var arraykvid = [];
     var text = "";
     getSqlresult("SELECT idkv_sudc from send_updates_to_dc GROUP BY idkv_sudc", function (result) {
-        text = "Hallo Leute folgende Kurse wurden neu eingetragen: \n\n\n"
+        text = "Hallo Leute folgende Kurse wurden neu eingetragen: \n\n"
         result.forEach(function (k) {
             arraykvid.push(k.idkv_sudc);
         });
@@ -51,17 +51,14 @@ var task = cron.schedule('0 * * * * 5', () => {
     });
 
 });
-
-
-
-
-//task.start();
+task.start();
 
 
 // Entscheidung was getan werden soll wenn eine Nachricht eingegeben wird 
 client.on("message", function (message) {
     //console.log(message);
     if (message.author.bot) return;
+
     if (!message.content.startsWith(prefix)) return;
 
 
@@ -70,119 +67,138 @@ client.on("message", function (message) {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command === "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-    }
-    else if (command === "sum") {
-        const numArgs = args.map(x => parseFloat(x));
-        const sum = numArgs.reduce((counter, x) => counter += x);
-        message.reply(`The sum of all the arguments you provided is ${sum}!`);
-    }
-    else if (command === "intro") {
-        message.reply('Hello');
-    }
-    else if (command === "codcw") {
-        message.reply('Hier ist mein battleTag für COD-COLDWAR:');
-    }// direkte Nachricht an den User
-    else if (command === "testme") {
-        message.author.send('test');
-        //message.reply('Hier ist mein battleTag für COD-COLDWAR:');
-    }
-    else if (command === "anmeldung") {
-        if (args !== '') {
-            var user = message.member;
-            checkifkursveranstaltungexists(args[0], function (result1) {
-                if (result1 === true) {
-                    getAnmeldungen(user, args[0], function (result2) {
-                        console.log(result2)
-                        if (result2 === false) {
-                            setAnmeldung(user, args[0], function (result3) {
-                            });
-                            message.reply('Du wurdest erfolgreich angemeldet!');
-                        } else {
-                            message.reply('Du bist bereits angemeldet für diesen Kurs!');
-                        }
-                    });
-                } else {
-                    message.reply('Diesen Kurs gibt es nicht!!!');
-                }
-            });
-
-            /*
-            con.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("discord");
-                var myobj = { _user: user.toString(), datum: args[0] };
-                dbo.collection("anmeldungen").insertOne(myobj, function (err, res) {
-                    if (err) throw err;
-                    console.log("1 Dokument eingetragen!");
-                    db.close();
+    switch (command) {
+        case "ping":
+            const timeTaken = Date.now() - message.createdTimestamp;
+            message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+            break;
+        case "intro":
+            message.reply(`HI`);
+            break;
+        case "anmelden":
+            if (args !== '') {
+                var user = message.member;
+                checkifkursveranstaltungexists(args[0], function (result1) {
+                    if (result1 === true) {
+                        getAnmeldungen(user, args[0], function (result2) {
+                            console.log(result2)
+                            if (result2 === false) {
+                                setAnmeldung(user, args[0], function (result3) {
+                                });
+                                message.reply('Du wurdest erfolgreich angemeldet!');
+                            } else {
+                                message.reply('Du bist bereits angemeldet für diesen Kurs!');
+                            }
+                        });
+                    } else {
+                        message.reply('Diesen Kurs gibt es nicht!!!');
+                    }
                 });
-            });
-            
-            message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
-            */
-        } else {
-            message.reply('Du hast keine Kursnummer angegeben');
-        }
-    }
-    else if (command === "abmelden") {
-        if (args !== '') {
-            var user = message.member;
-            checkifkursveranstaltungexists(args[0], function (result1) {
-                if (result1 === true) {
-                    getAnmeldungen(user, args[0], function (result2) {
-                        console.log(result2)
-                        if (result2 === true) {
-                            setAbmeldung(user, args[0], function (result3) {
-                            });
-                            message.reply('Du wurdest erfolgreich abgemeldet!');
-                        } else {
-                            message.reply('Du bist bereits abgemeldet für diesen Kurs!');
-                        }
-                    });
-                } else {
-                    message.reply('Diesen Kurs gibt es nicht!!!');
-                }
-            });
 
-            /*
-            con.connect(url, function (err, db) {
-                if (err) throw err;
-                var dbo = db.db("discord");
-                var myobj = { _user: user.toString(), datum: args[0] };
-                dbo.collection("anmeldungen").insertOne(myobj, function (err, res) {
+                /*
+                con.connect(url, function (err, db) {
                     if (err) throw err;
-                    console.log("1 Dokument eingetragen!");
-                    db.close();
+                    var dbo = db.db("discord");
+                    var myobj = { _user: user.toString(), datum: args[0] };
+                    dbo.collection("anmeldungen").insertOne(myobj, function (err, res) {
+                        if (err) throw err;
+                        console.log("1 Dokument eingetragen!");
+                        db.close();
+                    });
                 });
-            });
-            
-            message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
-            */
-        } else {
-            message.reply('Du hast keine Kursnummer angegeben');
-        }
-    } else if (command === "befehle") {
+                
+                message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
+                */
+            } else {
+                message.reply('Du hast keine Kursnummer angegeben');
+            }
+            break;
+        case "abmelden":
+            if (args !== '') {
+                var user = message.member;
+                checkifkursveranstaltungexists(args[0], function (result1) {
+                    if (result1 === true) {
+                        getAnmeldungen(user, args[0], function (result2) {
+                            if (result2 === true) {
+                                setAbmeldung(user, args[0], function (result3) {
+                                });
+                                message.reply('Du wurdest erfolgreich abgemeldet!');
+                            } else {
+                                message.reply('Du bist bereits abgemeldet für diesen Kurs!');
+                            }
+                        });
+                    } else {
+                        message.reply('Diesen Kurs gibt es nicht!!!');
+                    }
+                });
 
-        var user = message.member;
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db("discord");
-            dbo.collection("befehle").find({}).toArray(function (err, result) {
-                if (err) throw err;
-                var befehle = [];
-                result.forEach(element => {
-                    befehle.push(element.befehlcode);
-                })
-                message.channel.send("Das sind die nutzbaren befehle: " + befehle.join(", "));
-                db.close();
-            });
-        });
-        //message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
+                /*
+                con.connect(url, function (err, db) {
+                    if (err) throw err;
+                    var dbo = db.db("discord");
+                    var myobj = { _user: user.toString(), datum: args[0] };
+                    dbo.collection("anmeldungen").insertOne(myobj, function (err, res) {
+                        if (err) throw err;
+                        console.log("1 Dokument eingetragen!");
+                        db.close();
+                    });
+                });
+                
+                message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
+                */
+            } else {
+                message.reply('Du hast keine Kursnummer angegeben');
+            }
+            break;
+        case 'details':
+            if (args !== '') {
+                var text ="";
+                checkifkursveranstaltungexists(args[0], function (result1) {
+                    if (result1 === true) {
+                        var querysql = "SELECT * FROM kurs_veranstaltungen WHERE id_kv=" + args[0] + " AND deleted_kv = 0"
+                        getSqlresult(querysql, function (resultkv) {
+                            var von = moment(resultkv[0].von_kv).format('DD.MM.YYYY');
+                            var bis = moment(resultkv[0].bis_kv).format('DD.MM.YYYY');
+                            text += "Kursnummer: " + resultkv[0].id_kv + "\n Titel: " + resultkv[0].titel_kv + "\n" + "Beschreibung: " + resultkv[0].beschreibung_kv + "\n" + "VON - BIS: " + von + " - " + bis + "\n\n Module:\n\n";
+                            var querymodule = "SELECT * FROM modul_veranstaltungen WHERE idkv_mv=" + resultkv[0].id_kv;
+                            getSqlresult(querymodule,function(resultmodule){
+                                resultmodule.forEach(function (m) {
+                                    var mv_von = moment(m.von_mv).format('DD.MM.YYYY HH:MM:SS');
+                                    var mv_bis = moment(m.bis_mv).format('DD.MM.YYYY HH:MM:SS');
+                                    text += "Moduletitel: " + m.titel_mv + "\n" + "Beschreibung: " + m.beschreibung_mv + "\n" + "VON - BIS: " + mv_von + " - " + mv_bis + "\n\n";
+                                })
+                                message.author.send(text)
+                            });
+                        });
+                    } else {
+                        message.reply('Diesen Kurs gibt es nicht!!!');
+                    }
+                });
+
+                /*
+                con.connect(url, function (err, db) {
+                    if (err) throw err;
+                    var dbo = db.db("discord");
+                    var myobj = { _user: user.toString(), datum: args[0] };
+                    dbo.collection("anmeldungen").insertOne(myobj, function (err, res) {
+                        if (err) throw err;
+                        console.log("1 Dokument eingetragen!");
+                        db.close();
+                    });
+                });
+                
+                message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
+                */
+            } else {
+                message.reply('Du hast keine Kursnummer angegeben');
+            }
+            break;
+        default:
+            message.reply('Diesen Bedehl gibt es hier nicht!');
+            break;
     }
 });
+
 
 // Willkommen nachricht wenn neue Mitglieder dazu kommen
 client.on('guildMemberAdd', member => {
@@ -225,7 +241,7 @@ function getSqlresult(query, data) {
 
 function getAnmeldungen(user, kvid, data) {
     var queryresult = '';
-    con.query("SELECT COUNT('id_ka') as 'exist' FROM kursanmeldungen where iddc_ka = " + user + " AND idkv_ka = " + kvid +" AND deleted_ka IS NULL ", function (err, result, fields) {
+    con.query("SELECT COUNT('id_ka') as 'exist' FROM kursanmeldungen where iddc_ka = " + user + " AND idkv_ka = " + kvid + " AND deleted_ka IS NULL ", function (err, result, fields) {
         if (err) {
             throw err;
         }
@@ -253,7 +269,7 @@ function checkifkursveranstaltungexists(kvid, data) {
     })
 }
 
-function setAnmeldung(user, kvid, data) {
+function setAnmeldung(user, kvid) {
     con.query("Insert INTO kursanmeldungen ( iddc_ka , idkv_ka) VALUES ('" + user + "', " + kvid + ")", function (err, result, fields) {
         if (err) {
             throw err;
@@ -261,7 +277,7 @@ function setAnmeldung(user, kvid, data) {
     });
 }
 
-function setAbmeldung(user, kvid, data) {
+function setAbmeldung(user, kvid) {
     con.query("UPDATE kursanmeldungen SET deleted_ka =1 WHERE iddc_ka = '" + user + "' AND idkv_ka =  " + kvid + " AND deleted_ka IS NULL", function (err, result, fields) {
         if (err) {
             throw err;
